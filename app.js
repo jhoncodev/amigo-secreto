7132d17a40ba32c075f415fdc557ec2de7ca31f0
Jhon Codev <jhoncodev@gmail.com>
2025-02-26 15:54:32 -0500

    Array y función para agregar amigos creado

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = [];
+
+function addFriend(){
+    let friendName = document.getElementById('amigo').value;
+    
+    // Comprobar que no se ingrese un nombre vacío ni números
+    
+    if (friendName === '' || !isNaN(friendName)) {
+        alert("Ingrese un nombre válido");
+        return;
+    }else{
+        friends.push(friendName);
+        document.getElementById('amigo').value = '';
+        console.log(friends);
+    }
+}
