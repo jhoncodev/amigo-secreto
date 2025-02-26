c9484858b15617b15e4d005425fa7611ba74cb4d
Jhon Codev <jhoncodev@gmail.com>
2025-02-26 16:29:41 -0500

    Función para sortear amigoas creada

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function addFriend(){
     let friendName = document.getElementById('amigo').value;
     
     // Comprobar que no se ingrese un nombre vacío ni números
-    
+
     if (friendName === '' || !isNaN(friendName)) {
         alert("Ingrese un nombre válido");
         return;
@@ -12,5 +12,31 @@ function addFriend(){
         friends.push(friendName);
         document.getElementById('amigo').value = '';
         console.log(friends);
+        updateListFriends(friends);
+    }
+}
+
+function updateListFriends(friends){
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = '';
+
+    // Actualizar la lista de amigos
+
+    for (let index = 0; index < friends.length; index++) {
+        listaHTML.innerHTML += `<li>${friends[index]}</li>`;
+    }
+}
+
+function randomFriend(){
+
+    // Elegir un amigo aleatorio
+
+    if(friends.length > 0){
+        let random = Math.floor(Math.random() * friends.length);
+        let friend = document.getElementById('resultado');
+        document.getElementById('listaAmigos').innerHTML = '';
+        friend.innerHTML = `Tú amigo secreto es ${friends[random]}`;
+    }else{
+        alert("No hay amigos para elegir");
     }
 }
